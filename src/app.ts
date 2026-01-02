c87a58d3870c4bce2ef2394cff74acb587bac2fc
Pinaki Majumder <pinaki3.majumder@gmail.com>
2026-01-02 15:43:03 +0530

    feat: add CORS support to the application

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
+import customerRoutes from './routes/customer.routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true, // only if you use cookies / auth headers
+  }),
+);
+
 app.use('/api/auth', authRoutes);
+app.use('/api', customerRoutes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import app from './app';
 
+import cors from 'cors';
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true, // only if you use cookies / auth headers
+  }),
+);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
